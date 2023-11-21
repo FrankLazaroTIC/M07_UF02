2c4fa0dc832c54e77b3f1a037dd421388541d8fc
FrankLazaroTIC <2023_frank.lazaro@iticbcn.cat>
2023-11-21 19:48:19 +0100

    Practica 1 Frank

---
+++ my-project/app/Http/Controllers/SignController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SignController extends Controller
+{
+    function signIn($v1,$v2,$v3,$v4) {
+        $titol = $v1.' '.$v2.' '.$v3.' '.$v4;
+        return view('signIn')->with('titol',$titol);
+    }
+    
+    function signUp($v1,$v2,$v3) {
+        $titol = $v1.' '.$v2.' '.$v3;
+        return view('signUp')->with('titol',$titol);
+    }
+}
